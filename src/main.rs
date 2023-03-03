87b550b294f986e3677911ce49ee546647ef16f7
Suraj Mistry <mistrs8@gmail.com>
2023-03-03 16:47:15 -0500

    Initialize rust calculator app

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+// yt video @ 23:23 https://www.youtube.com/watch?v=MsocPEZBd-M
+use std::env::{args, Args};
+
+fn main() {
+    let mut args = args();
+
+    let first = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap().chars().next().unwrap();
+    let second = args.nth(0).unwrap();
+
+    let first_number= first.parse::<f32>().unwrap();
+    let second_number= second.parse::<f32>().unwrap();
+    let result = operate(operator, first_number, second_number);
+    println!("{:?}", result);
+}
+
+fn operate(operator: char, first_number: f32, second_number: f32) -> f32{
+    if operator == '+' {
+        first_number + second_number
+    } else if operator == '-' {
+        first_number - second_number
+    } else if operator == '/' {
+        first_number / second_number
+    } else if operator == '*' {
+        first_number * second_number
+    } else {
+        0.0
+    }
+}
